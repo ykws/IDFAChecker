2e68f7be467857768c1b5bef04b6ef94a1f9732e
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-03-21 21:48:59 +0900

    Initial Commit

---
+++ IDFAChecker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IDFAChecker
+//
+//  Created by KAWASHIMA Yoshiyuki on 2020/03/21.
+//  Copyright © 2020 ykws. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
